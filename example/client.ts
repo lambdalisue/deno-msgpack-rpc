0556731125c949d90afb2d824e30d13ee3b9c38c
Alisue <lambdalisue@hashnote.net>
2021-02-11 03:21:43 +0900

    Update example codes

--- example/client.ts
+++ example/client.ts
@@ -1,16 +1,16 @@
-import { Session, Dispatcher } from "../session.ts";
+import { Dispatcher, Session } from "../mod.ts";
 
 const hostname = "localhost";
 const port = 18800;
 
 const dispatcher: Dispatcher = {
-  async hello_server(name: string): Promise<string> {
+  async helloServer(name: unknown): Promise<unknown> {
     // NOTE: 'this' is an instance of Session
-    return this.call("hello_server", name);
+    return await this.call("hello_server", name);
   },
 
-  async hello_client(name: string): Promise<string> {
-    return `Hello ${name}, this is client`;
+  helloClient(name: unknown): Promise<unknown> {
+    return Promise.resolve(`Hello ${name}, this is client`);
   },
 };
 
@@ -23,8 +23,8 @@ try {
     .then(() => console.log("Session has disconnected"))
     .catch((e) => console.error(e));
   console.log(await client.call("sum", 1, 1));
-  console.log(await client.call("hello_server", "Bob"));
-  console.log(await client.call("hello_client", "Bob"));
+  console.log(await client.call("helloServer", "Bob"));
+  console.log(await client.call("helloClient", "Bob"));
   console.log(`Close connection`);
   conn.close();
 } catch (e) {
--- example/server.ts
+++ example/server.ts
@@ -1,33 +1,38 @@
-import { Session, Dispatcher } from "../session.ts";
+import { Dispatcher, Session } from "../mod.ts";
 
 const hostname = "localhost";
 const port = 18800;
 
 const dispatcher: Dispatcher = {
-  async sum(x: number, y: number): Promise<number> {
-    return x + y;
+  sum(x: unknown, y: unknown): Promise<unknown> {
+    if (typeof x !== "number" || typeof y !== "number") {
+      throw new Error("x and y must be number");
+    }
+    return Promise.resolve(x + y);
   },
 
-  async hello_server(name: string): Promise<string> {
-    return `Hello ${name}, this is server`;
+  helloServer(name: unknown): Promise<unknown> {
+    return Promise.resolve(`Hello ${name}, this is server`);
   },
 
-  async hello_client(name: string): Promise<string> {
+  async helloClient(name: unknown): Promise<unknown> {
     // NOTE: 'this' is an instance of Session
-    return this.call("hello_client", name);
+    return await this.call("hello_client", name);
   },
 };
 
-for await (const conn of Deno.listen({
-  hostname,
-  port,
-})) {
+for await (
+  const conn of Deno.listen({
+    hostname,
+    port,
+  })
+) {
   console.log("Session has connected");
   const server = new Session(conn, dispatcher);
   server
     .listen()
     .then(() => console.log("Client has disconnected"))
     .catch((e) => console.error(e));
-  console.log(await server.call("hello_server", "Alice"));
-  console.log(await server.call("hello_client", "Alice"));
+  console.log(await server.call("helloServer", "Alice"));
+  console.log(await server.call("helloClient", "Alice"));
 }
