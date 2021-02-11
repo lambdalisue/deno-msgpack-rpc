0650edf7855e80969d318ed7b85f7bf3362a3913
Alisue <lambdalisue@hashnote.net>
2021-02-11 09:21:27 +0900

    Use Deno Standard Library instead

--- session_test.ts
+++ session_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts";
-import { sleep } from "https://deno.land/x/sleep/mod.ts";
+import { delay } from "https://deno.land/std/async/mod.ts";
 import { Dispatcher, Session } from "./session.ts";
 import { createTransporter } from "./mod.ts";
 
@@ -23,7 +23,7 @@ class Reader implements Deno.Reader, Deno.Closer {
         p.set(v);
         return v.length;
       }
-      await sleep(0.001);
+      await delay(1);
     }
     return null;
   }
