32a0ae6f81e9c397bda90f5f57dd2cfce7244f11
Alisue <lambdalisue@hashnote.net>
2021-04-19 13:25:14 +0900

    Update msgpack to v1.4 to fix #11

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { decodeStream, encode } from "https://deno.land/x/msgpack@v1.3/mod.ts";
+export { decodeStream, encode } from "https://deno.land/x/msgpack@v1.4/mod.ts";
 export { deferred } from "https://deno.land/x/std@0.93.0/async/deferred.ts";
 export * as io from "https://deno.land/x/std@0.93.0/io/mod.ts";
 export type {
