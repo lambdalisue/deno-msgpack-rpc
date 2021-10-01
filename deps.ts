be25e25ccceff9205a8d21108c63ff4c937d4a9b
GitHub <noreply@github.com>
2021-10-01 00:23:48 +0000

    :package: Update deno dependencies

--- deps.ts
+++ deps.ts
@@ -2,4 +2,4 @@ export { decodeStream, encode } from "https://deno.land/x/msgpack@v1.4/mod.ts";
 export { deferred, delay } from "https://deno.land/x/std@0.109.0/async/mod.ts";
 export * as io from "https://deno.land/x/std@0.109.0/io/mod.ts";
 export type { Deferred } from "https://deno.land/x/std@0.109.0/async/mod.ts";
-export type { Disposable } from "https://deno.land/x/disposable@v1.0.0/mod.ts";
+export type { Disposable } from "https://deno.land/x/disposable@v1.0.1/mod.ts";
